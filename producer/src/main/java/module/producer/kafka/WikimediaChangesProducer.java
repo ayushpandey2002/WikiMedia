1821199375bee07306dada2335caa7d8fcd1932a
AYUSH PRAKASH PANDEY <85828536+ayushpandey2002@users.noreply.github.com>
2024-02-27 16:10:12 +0530

    first commit

---
+++ producer/src/main/java/module/producer/kafka/WikimediaChangesProducer.java
@@ -0,0 +1,38 @@
+package module.producer.kafka;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+
+import lombok.RequiredArgsConstructor;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+
+public class WikimediaChangesProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WikimediaChangesProducer.class);
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    @Value("${spring.kafka.producer.topic}")
+    private String topic;
+
+    public void produce() throws InterruptedException {
+        LOGGER.info("Producing...");
+
+        EventHandler eventHandler =  new WikimediaChangesHandler(kafkaTemplate, topic);
+        final String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource eventSource = new EventSource.Builder(eventHandler, URI.create(url)).build();
+        eventSource.start();
+        Duration.ofMinutes(10);
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
